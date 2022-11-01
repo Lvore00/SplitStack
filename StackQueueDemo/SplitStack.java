03e220a7c0874597693f46cd71408b7fb893e5a5
Lilly <111086892+Lvore00@users.noreply.github.com>
2022-11-01 17:09:34 -0400

    Create SplitStack.java

---
+++ StackQueueDemo/SplitStack.java
@@ -0,0 +1,67 @@
+package StackQueueDemo;
+
+/*
+Name: Lillian Vore
+Date: 11/2/2022
+Class: Intermediate Prog.
+Instructions: Write a called SplitStack.java.
+        Write a method splitStack that takes a stack of integers as a parameter and splits it into
+        negatives and non-negatives. The numbers in the stack should be rearranged so that all the
+        negatives appear on the bottom of the stack and all the non-negatives appear on the top. In
+        other words, if after this method is called you were to pop numbers off the stack, you would
+        first get all the non negative numbers and then get all the negative numbers. It does not
+        matter what order the numbers appear in as long as all the negatives appear lower in the
+        stack than all the non-negatives. You may use a single queue as auxiliary storage
+*/
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class SplitStack {
+
+    public static void modify(Stack<Integer> s){
+        Queue<Integer> t = new LinkedList<Integer>();
+        int count = 0;
+        while(s.size() != 0){
+            Integer temp = s.pop();
+            t.add(temp);
+            if(temp.intValue() >= 0){
+                count++;
+            }
+        }
+        while(t.size() != 0){
+            Integer temp = t.remove();
+            if(temp.intValue() < 0){
+                if(s.size() >= count){
+                    s.push(temp);
+                }
+                else{
+                    t.add(temp);
+                }
+            }
+            else{
+                s.push(temp);
+            }
+        }
+    }
+
+    public static void main(String args[]){
+        Stack<Integer> s = new Stack<Integer>();
+        s.push(new Integer(5));
+        s.push(new Integer(-5));
+        s.push(new Integer(67));
+        s.push(new Integer(-45));
+        s.push(new Integer(67));
+        s.push(new Integer(9));
+        s.push(new Integer(0));
+        s.push(new Integer(-42));
+        s.push(new Integer(56));
+        s.push(new Integer(-7));
+        System.out.println(s);
+        modify(s);
+        System.out.println(s);
+    }
+}
+
