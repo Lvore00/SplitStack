dd6226c479862b589048ae1ee8fac7ce3a1fafa0
Lilly <111086892+Lvore00@users.noreply.github.com>
2022-11-02 16:42:00 -0400

    Update SplitStack.java

--- StackQueueDemo/SplitStack.java
+++ StackQueueDemo/SplitStack.java
@@ -49,25 +49,20 @@ public class SplitStack {
 
         public static void main(String args[]){
             Stack<Integer> s = new Stack<Integer>();
-            //Integer(int) is deprecated so please rewrite the belows
-            // like this s.push(5)  or s.push(Integer.valueOf(5));
-            // as I am providing an example below.
-            s.push(5); //this is an example.
-            s.push(-5); //revise rest as above.
-            s.push(67);//revise rest as above.
-            s.push(-45);//revise rest as above.
-            s.push(67);//revise rest as above.
-            s.push(9);//revise rest as above.
-            s.push(0);//revise rest as above.
-            s.push(-42);//revise rest as above.
-            s.push(56);//revise rest as above.
-            s.push(-7);//revise rest as above.
+       
+            s.push(5); 
+            s.push(-5); 
+            s.push(67);
+            s.push(-45);
+            s.push(67);
+            s.push(9);
+            s.push(0);
+            s.push(-42);
+            s.push(56);
+            s.push(-7);
             System.out.println(s);
             modify(s);
-            System.out.println(s); //also output must have negative on the left
-            // and positive on the right side.  Lower stack means you will need
-            // to display the negative number on your left. Please revise your
-            // program accordingly.
-
+            System.out.println(s);
+            
         }
     }
